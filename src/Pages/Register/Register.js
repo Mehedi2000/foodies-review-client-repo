0a754d02428ba5ee1d0982861a3aa3fc42d45d38
Mehedi Hasan <mh4054528@gmail.com>
2022-11-08 22:40:36 +0600

    added register

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const LogIn = () => {
+const Register = () => {
   return (
     <div className="hero min-h-screen bg-base-200 my-10 w-3/4 mx-auto rounded-2xl">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
-            <h2 className="text-center text-3xl font-semibold">Login !</h2>
+            <h2 className="text-center text-3xl font-semibold">Register !</h2>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Your Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                className="input input-bordered"
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -31,16 +42,12 @@ const LogIn = () => {
               />
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
-            </div>
-            <p className="text-xl text-center font-medium">Or</p>
-            <div className="form-control">
-              <button className="btn btn-primary">Google</button>
+              <button className="btn btn-primary">Register</button>
             </div>
             <p className="text-xl font-bold text-center">
-              New User?
-              <Link to="/register" className="text-primary">
-                Register
+              Already Have an Account?
+              <Link to="/login" className="text-primary">
+                LogIn
               </Link>
             </p>
           </div>
@@ -50,4 +57,4 @@ const LogIn = () => {
   );
 };
 
-export default LogIn;
+export default Register;
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import LogIn from "../../Pages/LogIn/LogIn";
+import Register from "../../Pages/Register/Register";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: "/login",
         element: <LogIn></LogIn>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
